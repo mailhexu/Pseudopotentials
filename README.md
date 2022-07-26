7e7eed807025e32f7d5da84054b175b04ed9473f
hexu <mailhexu@gmail.com>
2022-07-26 14:39:29 +0200

    add pseudodojo psml f-elements

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Pseudopotentials
-Collection of pseudopotentials
+Collection of pseudopotentials.
+
+WARNING: The pseudopotentials here should be used with care. Always test before usage!
+
+
+PSML PPs for f-elements from pseudodojo input files in the github repo. The functional PBE and PBEsol are complete. For the PW functional only La and Lu are included. 
+For other elements, they can be found on the Pseudodojo website:
+http://www.pseudo-dojo.org/
+
+.
+└── f_electrons_dojo 
+    ├── PPs
+    │   ├── PBE_FR    
+    │   ├── PBEsol_FR
+    │   ├── PBEsol_SR
+    │   ├── PBE_SR
+    │   ├── PW_FR
+    │   └── PW_SR
+    └── script
+
---
+++ psml_f_electrons_dojo/script/gen_f_pseudos.py
@@ -0,0 +1,82 @@
+import os
+import glob
+from ase.data import chemical_symbols
+import shutil
+
+root = '/home/hexu/projects/dojo/pseudo_dojo-master/pseudo_dojo/pseudos'
+elems = chemical_symbols[57:72]
+"""
+for i in Co N C O H
+do
+oncvpspr.x < $i.in
+cp ONCVPSPPSML $i.psml
+done
+"""
+
+
+def find_input(elem, xc):
+    epath = f"ONCVPSP-{xc}-PDv0.4/{elem}"
+    path = os.path.join(root, epath)
+    if not os.path.exists(path):
+        return []
+    else:
+        return glob.glob(f"{root}/{epath}/{elem}*.in")
+
+
+def get_input_sp(xc, elem, label="-sp.in"):
+    epath = f"ONCVPSP-{xc}-PDv0.4/{elem}"
+    fname = f"{root}/{epath}/{elem}{label}"
+    if os.path.exists(fname):
+        return fname
+
+
+def gen_psml(xc, elem, label="-sp.in", rel='SR'):
+    command = {'NR': 'oncvpspnr.x', 'SR': 'oncvpsp.x', "FR": 'oncvpspr.x'}
+    cache_path = os.path.join(root, 'cache', xc, elem)
+    os.makedirs(cache_path, exist_ok=True)
+    fname = get_input_sp(xc, elem, label)
+    path_nr = os.path.join(root, 'pps', f"{xc}_{rel}")
+    os.makedirs(path_nr, exist_ok=True)
+    failed = open("failed.txt", 'w')
+    if fname is not None:
+        shutil.copy(fname, cache_path)
+        os.chdir(cache_path)
+        os.system("rm ./*")
+        os.system(f"{command[rel]} < {fname} > output")
+        name = os.path.join(path_nr, f"{elem}.psml")
+        print(os.path.join(path_nr, f"{elem}.psml"))
+        psml_fname = os.path.join(cache_path, "ONCVPSPPSML")
+        if os.path.exists(psml_fname):
+            shutil.copy(psml_fname,
+                        os.path.join(path_nr, f"{elem}.psml"))
+            shutil.copy(os.path.join(cache_path, "output"),
+                        os.path.join(path_nr, f"{elem}.out"))
+        else:
+            failed.write(f"{name}\n")
+            print(f"{name}: Failed")
+
+    failed.close()
+
+
+for xc in ['PBE', 'PBEsol', 'PW']:
+    for elem in elems:
+        gen_psml(xc, elem, rel='NR')
+        gen_psml(xc, elem, rel='SR')
+        gen_psml(xc, elem, rel='FR')
+
+
+def get_psml(elem, xc):
+    cache_path = os.path.join(root, 'cache', xc, elem)
+    os.makedirs(cache_path, exist_ok=True)
+    # shutil.copy(src=os.path.)
+
+
+def test():
+    xc = "PBE"
+    for elem in elems:
+        inputs = find_input(elem, xc)
+        for inp in inputs:
+            get_psml(elem, xc)
+
+
+# test()
